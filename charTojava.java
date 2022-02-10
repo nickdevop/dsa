0264a20c7a45187c83dc197f3e021eeab021ff1c
nickdevop <2019621103.nitin@ug.sharda.ac.in>
2022-02-10 18:57:15 +0530

    char

---
+++ charTojava.java
@@ -0,0 +1,23 @@
+package javadsa;
+
+import java.util.Scanner;
+
+public class charTojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		 Scanner sc = new Scanner(System.in); 
+		   
+	        // Character input 
+	        char c = sc.next().charAt(0); 
+	   
+	        // Print the read value 
+	        System.out.println("c = "+c); 
+	    } 
+	
+		
+	}
+
+
